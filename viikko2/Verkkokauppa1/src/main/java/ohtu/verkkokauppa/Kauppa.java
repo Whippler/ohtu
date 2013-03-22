f8d45fddfd07a89cb8c14dbbcb0767f81294fa76
lammenoj <lammenoj@dcb-25.cs.helsinki.fi>
2013-03-22 17:48:52 +0200

    Tehtävä 4, paitsi luokkakaavio.

--- viikko2/Verkkokauppa1/src/main/java/ohtu/verkkokauppa/Kauppa.java
+++ viikko2/Verkkokauppa1/src/main/java/ohtu/verkkokauppa/Kauppa.java
@@ -1,6 +1,6 @@
 package ohtu.verkkokauppa;
 
-public class Kauppa {
+public class Kauppa implements KauppaInterface {
 
     private Varasto varasto;
     private Pankki pankki;
@@ -15,15 +15,25 @@ public class Kauppa {
         kaupanTili = "33333-44455";
     }
 
+    public Kauppa(Varasto instance, Pankki instance0, Viitegeneraattori instance1) {
+        //throw new UnsupportedOperationException("Not yet implemented");
+        this.varasto = instance;
+        this.pankki = instance0;
+        this.viitegeneraattori = instance1;
+    }
+
+    @Override
     public void aloitaAsiointi() {
         ostoskori = new Ostoskori();
     }
 
+    @Override
     public void poistaKorista(int id) {
         Tuote t = varasto.haeTuote(id); 
         varasto.palautaVarastoon(t);
     }
 
+    @Override
     public void lisaaKoriin(int id) {
         if (varasto.saldo(id)>0) {
             Tuote t = varasto.haeTuote(id);             
@@ -32,6 +42,7 @@ public class Kauppa {
         }
     }
 
+    @Override
     public boolean tilimaksu(String nimi, String tiliNumero) {
         int viite = viitegeneraattori.uusi();
         int summa = ostoskori.hinta();
